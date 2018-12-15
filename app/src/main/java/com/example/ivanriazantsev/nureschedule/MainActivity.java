5dac8e2d70d3f0bce3140df2ec29afb659bcb7ed
IvanRiazantsev <ivan.riazantsev@gmail.com>
2018-12-15 06:10:30 +0200

    added toolbar

--- app/src/main/java/com/example/ivanriazantsev/nureschedule/MainActivity.java
+++ app/src/main/java/com/example/ivanriazantsev/nureschedule/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.ivanriazantsev.nureschedule;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+
     }
 }
